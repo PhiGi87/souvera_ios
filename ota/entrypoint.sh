23a455c9162df8c99ce70351a7f7bd580ebc7381
root <root@056789b1749b>
2026-07-24 10:32:17 +0000

    fix(ota): serve IPA from GitHub Releases CDN, remove entrypoint/download

--- ota/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-echo "Downloading IPA..."
-curl -fsSL -o /usr/share/nginx/html/Souvera.ipa \
-  "https://github.com/PhiGi87/souvera_ios/releases/download/ota-33.1.0/Nextcloud.ipa" && \
-echo "IPA ready ($(du -h /usr/share/nginx/html/Souvera.ipa | cut -f1))"
-exec nginx -g "daemon off;"
